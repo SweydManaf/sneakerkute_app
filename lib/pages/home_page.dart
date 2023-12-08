6d5aebb49f487211c328ddefff70a9263cc1a84f
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2023-12-08 07:28:17 +0200

    bottom navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_app/components/bottom_nav_bar.dart';
+import 'package:ecommerce_app/pages/cart_page.dart';
+import 'package:ecommerce_app/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +11,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+  // when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
   }
 }
